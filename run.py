d8e378931d66fab7e71d9b1cf462167d0c4447fb
pyvsu <140031903+pyvsu@users.noreply.github.com>
2023-11-30 06:06:41 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,25 @@
 import main
+import os
 
-while True:
-    text = input('_____ MASA _____ \n')
-    result, error = main.run('<stdin>', text)
+# Define the allowed file extension
+allowed_extension = ".ms"
 
-    if error: print(error.as_string())
-    else: print(result)
+# Check if the file exists and has the correct extension
+file_path = 'test.ms'  
+if os.path.isfile(file_path) and file_path.endswith(allowed_extension):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()  # Read all lines from the file
+
+    # Iterate through each line in the file
+    for line in lines:
+        line = line.strip()  
+        result, error = main.run('<stdin>', line)
+
+        if error:
+            print(error.as_string())
+        else:
+            print(result)
+else:
+    print(f"File '{file_path}' either doesn't exist or doesn't have the '{allowed_extension}' extension.")
+
+
